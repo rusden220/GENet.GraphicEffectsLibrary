3b471fff1bfa197a2d25b0e66f3c6f8e78e3ca41
rusden220 <surin.denis.k@gmail.com>
2016-09-13 13:17:51 +0300

    first base effects

---
+++ GENet.GraphicEffectsLibrary/Effects/EffectBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GENet.GraphicEffectsLibrary.Effects
+{
+	/// <summary>
+	/// desc here
+	/// </summary>
+	public class EffectBase
+	{
+	}
+}
